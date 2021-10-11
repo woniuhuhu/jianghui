944a27823abddb310cfd2fb49d3b297289605dc6
woniuhuhu <jh18205234513@163.com>
2021-10-11 05:51:29 +0000

    3-16

--- service/gateway/init.lua
+++ service/gateway/init.lua
@@ -20,6 +20,44 @@ local connect = function ( fd,addr )
     skynet.fork(recv_loop,fd)
 end
 
+local recv_loop = function(fd)
+	socket.start(fd)
+	skynet.error("socket connect"..fd)
+	local readbuff = ""
+	while true do
+		local recvstr = socket.read(fd)
+		if recvstr then
+			readbuff = readbuff..recvstr
+			readbuff = process_buff(fd,readbuff)
+		else
+			skynet.error("skoket close"..fd)
+			disconnect(fd)
+			socket.close(fd)
+			return
+		end
+	end
+end
+
+local process_buff = function(fd,readbuff)
+	while true do
+		local msgstr,rest = string.match(readbuff,"(.-)\r\n(.*)")
+		if msgstr then
+			readbuff = rest
+			process_msg(fd,msgstr)
+		else
+			return readbuff
+		end
+	end		
+end
+local process_msg = function(fd,msgstr)
+	print(msgstr)
+end
+
+
+
+
+
+
 conns = {} --[fd] = conn
 players = {} --[playerid] = gateplayer
 --连接类
@@ -28,7 +66,7 @@ function conn()
         fd = nil,
         player = nil,
     }
-    return m}
+    return m
 end
 
 function gateplayer()
